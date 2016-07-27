03e8de1c886896aad7632402bb073e52b016c55d
mosampaio <mosampaio@gmail.com>
2016-07-27 10:53:59 -0300

    main page spec

--- spec/routes/index.spec.js
+++ spec/routes/index.spec.js
@@ -2,12 +2,33 @@
 
 var expect = require('chai').expect
   , supertest = require('supertest')
+  , cheerio = require('cheerio')
   , app = require('../../app.js');
 
 describe('default route', function () {
   describe('GET /', function () {
     it('responds with 200 OK', function (done) {
-      supertest(app).get('/').expect(200, done);
+      supertest(app)
+      .get('/')
+      .expect(function (res) {
+          var $ = cheerio.load(res.text);
+          expect($('.well div h1').text()).to.equal('Don\'t have an account?');
+          expect($('button.btn').text()).to.equal('Log in');
+          expect($('a.btn').text()).to.contain('Sign up');
+          var text = 'This is a demonstration of how to add TOTP based ' +
+                      'Two-Factor Authentication to an existing application.';
+          expect($('.span6 div').first().text()).to.equal(text);
+        })
+      .expect(200, done);
     });
   });
+
 });
+
+// describe('main page', function () {
+//   describe('GET /', function () {
+//     it('responds with 200 OK', function (done) {
+//       supertest(app).get('/').expect(200, done);
+//     });
+//   });
+// });
