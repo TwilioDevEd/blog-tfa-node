bf793f9548760ec1c7a0fdce58c39d77daacfb30
mosampaio <mosampaio@gmail.com>
2016-08-01 12:13:47 -0300

    Parameterize app secret key

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var config = require('./config');
 
 var app = express();
 
@@ -12,15 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'your-secret',
+  secret: config.secret,
   resave: true,
   saveUninitialized: true
 }));
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var config = {};
+var _ = require('underscore');
 
 var dbConnection = function() {
   if (process.env.NODE_ENV === 'test') {
@@ -10,6 +10,21 @@ var dbConnection = function() {
   return 'mongodb://localhost/blog-tfa';
 };
 
-config.dbConnection = dbConnection();
+var config = {
+  dbConnection: dbConnection(),
+  secret: process.env.APP_SECRET,
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  phoneNumber: process.env.TWILIO_NUMBER
+};
+
+var isUnconfigured = _.every(_.values(config), function(value) {
+  return _.isUndefined(value);
+});
+
+if (isUnconfigured) {
+  throw new Error(
+    'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER, and APP_SECRET must be set');
+}
 
 module.exports = config;
