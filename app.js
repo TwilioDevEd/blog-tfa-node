4b37acb65cfd383a0a3f1f8b324c273a1314287f
mosampaio <mosampaio@gmail.com>
2016-08-24 13:43:16 -0300

    Remove static prefix from urls

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: config.secret,
   resave: true,
